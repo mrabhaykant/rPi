bef0556afd083f8d5940626e7c9888e666a8174e
mrabhaykant <53424910+mrabhaykant@users.noreply.github.com>
2020-12-05 23:12:30 +0530

    Add files via upload

---
+++ Basic C Code/swapping using left shift and right shift/main.s
@@ -0,0 +1,48 @@
+	.arch armv6
+	.eabi_attribute 28, 1
+	.eabi_attribute 20, 1
+	.eabi_attribute 21, 1
+	.eabi_attribute 23, 3
+	.eabi_attribute 24, 1
+	.eabi_attribute 25, 1
+	.eabi_attribute 26, 2
+	.eabi_attribute 30, 6
+	.eabi_attribute 34, 1
+	.eabi_attribute 18, 4
+	.file	"main.c"
+	.text
+	.align	2
+	.global	main
+	.arch armv6
+	.syntax unified
+	.arm
+	.fpu vfp
+	.type	main, %function
+main:
+	@ args = 0, pretend = 0, frame = 8
+	@ frame_needed = 1, uses_anonymous_args = 0
+	@ link register save eliminated.
+	str	fp, [sp, #-4]!
+	add	fp, sp, #0
+	sub	sp, sp, #12
+	mov	r3, #18
+	strb	r3, [fp, #-5]
+	ldrb	r3, [fp, #-5]	@ zero_extendqisi2
+	lsl	r3, r3, #4
+	sxtb	r2, r3
+	ldrb	r3, [fp, #-5]	@ zero_extendqisi2
+	lsr	r3, r3, #4
+	uxtb	r3, r3
+	sxtb	r3, r3
+	orr	r3, r2, r3
+	sxtb	r3, r3
+	strb	r3, [fp, #-5]
+	mov	r3, #0
+	mov	r0, r3
+	add	sp, fp, #0
+	@ sp needed
+	ldr	fp, [sp], #4
+	bx	lr
+	.size	main, .-main
+	.ident	"GCC: (Raspbian 8.3.0-6+rpi1) 8.3.0"
+	.section	.note.GNU-stack,"",%progbits
